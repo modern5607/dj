fd36ff40791e16c63c88bdfae180fec3678dc937
tjs5002 <tjs0651@naver.com>
2021-01-25 09:06:14 +0900

    tail update

--- application/views/layout/tail.php
+++ application/views/layout/tail.php
@@ -49,7 +49,23 @@ $(".mshow").on("click",function(){
 
 });
 
+$(".menu01_li").on("click",function(){
+	
+	$(".menu01_li").find(".menu_a").removeClass("on");
+	$(".menu01_li").find("ul").css("display","none");
 
+	$(this).find(".menu_a").addClass("on");
+	$(this).find("ul").css("display","block");
+});
+
+//input autocoamplete off
+$("input"). attr("autocomplete", "off"); 
+
+//pageignation
+$(".limitset select").on("change", function() {
+    var qstr = "<?php echo $qstr ?>";
+    location.href = "<?php echo base_url($pos."/".$subpos)?>" + qstr + "&perpage=" + $(this).val();
+});
 
 </script>
 
