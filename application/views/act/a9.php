e5945ac7f0d196d9c1eda30334620e1e028eff6c
DESKTOP-ENT7MMV\고동민 <wk3221@gmail.com>
2021-01-29 09:45:19 +0900

    number

--- application/views/act/a9.php
+++ application/views/act/a9.php
@@ -128,7 +128,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <tr style="background:#f3f8fd;">
                                         <td colspan="2" style="text-align:right;"><strong><?php echo $row->SERIES_NM; ?></strong></td>
                                         <td></td>
-                                        <td class="right"><strong><?php echo $row->IN_QTY; ?></strong></td>
+                                        <td class="right"><strong><?php echo number_format($row->IN_QTY); ?></strong></td>
                                         <td></td>
                                         <td></td>
                                     </tr>
@@ -140,7 +140,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         <td class="cen"><?php echo $num; ?></td>
                                         <td><?php echo $row->SERIES_NM; ?></td>
                                         <td><?php echo $row->ITEM_NAME; ?></td>
-                                        <td class="right"><?php echo $row->IN_QTY; ?></td>
+                                        <td class="right"><?php echo number_format($row->IN_QTY); ?></td>
                                         <td><?php echo $row->REMARK;?></td>
                                         <td><span class="btn del_items"
                                         data-idx="<?php echo $row->TRANS_IDX; //detail idx?>" 
