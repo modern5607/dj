72e7a303c5f04f0fe15d7287dc70e58389773a5b
tjs5002 <tjs0651@naver.com>
2021-02-02 16:03:20 +0900

    Revert "Update ajax_p2_set.php"
    This reverts commit 65308943005a39190c8931284464b147f7504a9e.

--- application/views/pln/ajax_p2_set.php
+++ application/views/pln/ajax_p2_set.php
@@ -20,7 +20,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			
 			<fieldset class="form_3">
 				
-				<table class="nhover">
+				<table>
 					<tbody>
 						<tr>
 							<th>날짜<span class="re"></span></th>
