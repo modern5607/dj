32f1b75f1aacf6d60e647a951c5ec4566a191910
DESKTOP-ENT7MMV\고동민 <wk3221@gmail.com>
2021-03-16 10:16:58 +0900

    table size
    dm

--- application/views/tablet/o4.php
+++ application/views/tablet/o4.php
@@ -89,7 +89,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
             </div>
 
             <div class="tbl-content-t">
-                <table class="t_table" cellpadding="0" cellspacing="0" border="0" width="100%">
+                <table class="t_table_s" cellpadding="0" cellspacing="0" border="0" width="100%">
                     <thead>
                         <tr>
                             <th>No</th>
